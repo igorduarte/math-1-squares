e0402765cd1cbd971e6d3872cbde099d21c42acd
Igor Duarte <igor.augustodr@gmail.com>
2017-10-17 21:41:43 -0200

    Desafio completo

--- lib/quadrados.rb
+++ lib/quadrados.rb
@@ -1,2 +1,23 @@
+require 'prime'
+
 class Quadrados
+  def initialize(n)
+    @n = n
+  end
+
+  def quadrado_da_soma
+    a = Prime.first(@n)
+    a.sum ** 2
+  end
+
+  def soma_dos_quadrados
+    a = Prime.first(@n)
+    b = []
+    a.each { |n| b << n**2  }
+    b.sum
+  end
+
+  def diferenca
+    quadrado_da_soma - soma_dos_quadrados
+  end
 end
